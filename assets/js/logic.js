1b0e6611c8d0b73e7b61b59079479c81517c47bb
nukte-y <nukteu@gmail.com>
2024-01-15 11:12:07 +0000

    added answer buttons and eventlistener for them

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,43 +1,82 @@
 var startScreen=document.getElementById("start-screen");
+var endScreen=document.getElementById("end-screen");
 var questionTitle=document.getElementById("question-title");
-var question=document.querySelector(".hide"); 
+var question=document.getElementById("questions");
 var choices=document.getElementById("choices"); 
 var timeEl = document.querySelector(".timer");
 var secondsLeft = 30;
-var index;
+var index=0;
+var correct=0;  
+var wrong=0;
+var correctSound=(document.createElement("AUDIO").setAttribute("src","./assets/sfx/correct.wav"));   
+var incorrectSound=document.createElement("AUDIO");
 
-function setTime() {
-    // Sets interval in variable
+
+
+function setTime() {                
     var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.textContent=secondsLeft;
+        secondsLeft--;
+        timeEl.textContent=secondsLeft;
   
-      if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-      }
-  
-    }, 1000);
-  }
+        if(secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+        }
+    }, 1000)
+};
 
-function cleaner(){           //clean the screen
-    startScreen.textContent="";
-}
-  
+
+function cleaner(element){           //clean the screen
+    element.textContent="";
+};
+
+
+function block(element){             //make elements visible
+    element.style.display="block";   
+};
+
+
+function Button(event){  
+
+    var Select=event.target.id;     //get id of the clicked button    
+    if(Select===Questions[index].correctAnswer){
+        correct++;
+        // correctSound.play();
+    }
+    else{
+
+    };
+    
+    index++;
+    choices=document.getElementById("choices");
+    cleaner(choices);               // to clean previous answer buttons  
+    if(index===4){                  // if it is last question
+        cleaner(question);                        
+        block(endScreen);
+        return;
+    }
+    
+    QuestionRender();
+};
+
+
 function QuestionRender(){         //display questions on the screen 
-    index=0;
-    question.style.display="block";     //make questions visible(they are hidden in the html)
-    questionTitle.textContent=Questions[0].Q;
-    choices.textContent={
-        A: Questions[0].A,
-        B: Questions[0].B,
-        C: Questions[0].C,    
-        };
-        
-    } 
 
-document.getElementById("start").addEventListener("click",function(event){
-    cleaner();
+    block(questions);                //make questions visible(they are hidden in the html)
+    questionTitle.textContent=Questions[index].Q;
+        
+    var answers=Questions[index].Answers;   //takes answers object, from questions array 
+    for (const key in answers) {        //loop inside the object
+        var button=choices.appendChild(document.createElement("button"));   //create button under question div, for each object element    
+        button.textContent=answers[key];
+        button.id = key;
+        button.addEventListener("click",Button);
+    }        
+} 
+
+document.getElementById("start").addEventListener("click",function(event){          //start click
+    event.preventDefault();    
+    cleaner(startScreen);
     setTime();
     QuestionRender();
 
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,32 +1,35 @@
 var Questions=[ 
     {
     Q: "What is the capital of Japan?",
+    Answers: {
     A: "Kyoto",
     B: "Tokyo",
-    C: "Seoul",
+    C: "Seoul",},
     correctAnswer: "B",    
     },
     {    
     Q: "Which planet known as Red Planet?",
+    Answers: {
     A: "Mars",
     B: "Jupiter",
-    C: "Saturn",
+    C: "Saturn",},
     correctAnswer: "A",
     },
     {    
     Q: "Who wrote Romeo and Juliet?",
+    Answers: {
     A: "Jane Austen",
     B: "Charles Dickens",
-    C: "William Shakespeare",
+    C: "William Shakespeare",},
     correctAnswer:"C",   
     },
     {   
     Q: "Which of the following is a breed of cat?",
+    Answers: {
     A: "Labrador",
     B: "British Shorthair",
-    C: "Poodle",
+    C: "Poodle",},
     correctAnswer:"B",   
     },
 ];
-
 
